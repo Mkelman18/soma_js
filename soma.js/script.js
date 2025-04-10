c78f53bf12307302c5ddb4c055a6c65d6fa721b4
Mkelman18 <160253161+Mkelman18@users.noreply.github.com>
2025-04-10 10:23:23 -0300

    Add files via upload

---
+++ soma.js/script.js
@@ -0,0 +1,28 @@
+
+function somar (num1,num2){
+    alert (num1+num2);
+}
+
+function substracao (num1,num2){
+    alert (num1-num2);
+}
+
+function somar (num1,num2){
+    alert (num1*num2);
+}
+
+function somar (num1,num2){
+    alert (num1/num2);
+}
+
+alert ("Calculadora");
+let primeiroNumero=Number(prompt("Digite o primeiro Número!"));
+let segundoNumero=Number(prompt("Digite o segundo Número!"));
+
+somar (primeiroNumero,segundoNumero);
+
+substracao (primeiroNumero,segundoNumero);
+
+multiplicacao (primeiroNumero,segundoNumero);
+
+divisao (primeiroNumero,segundoNumero); 
